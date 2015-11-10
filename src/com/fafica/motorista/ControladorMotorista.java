709b3f2a41f6429e546c9288e1f8aaaca6d53b82
Ribeiro <Ribeiro@192.168.25.2>
2015-11-10 19:47:24 -0200

    Vamos começar, ja comecei implementando minhas entidades Motorista e viangens e os repositorios delas.

--- src/com/fafica/motorista/ControladorMotorista.java
+++ src/com/fafica/motorista/ControladorMotorista.java
@@ -1,7 +1,18 @@
 package com.fafica.motorista;
+import java.util.ArrayList;
+
 
 public class ControladorMotorista {
 	
-	private IRepositorioMotorista repositorioMotorista; 
+	private IRepositorioMotorista repositorioMotorista;
+	
+	
+	public ControladorMotorista(){
+		
+		this.repositorioMotorista = new RepositorioMotoristaArrayList();
+	}
 
+	public void cadastrar(Motorista morista){
+		
+	}
 }
